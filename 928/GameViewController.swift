49dddbf3b128b2c5b62b860f2bb723762986ab36
fAyano <75403424+fAyano@users.noreply.github.com>
2022-01-07 23:53:10 +0900

    プロトコルの実装
    爆弾ボタンを押すと人狼のキャラクターが一時的に消えてしまうので後日修正する必要がある

--- 928/GameViewController.swift
+++ 928/GameViewController.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-import Combine//タイマーを作る
+//import Combine//タイマーを作る
 
 class GameViewController: SuperGameViewController {
     @IBOutlet weak var upbutton: UIButton!
@@ -14,6 +14,7 @@ class GameViewController: SuperGameViewController {
     @IBOutlet weak var downbutton: UIButton!
     @IBOutlet weak var leftbutton: UIButton!
     @IBOutlet weak var bombbutton: UIButton!
+    @IBOutlet weak var jinro: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,18 +23,48 @@ class GameViewController: SuperGameViewController {
 
     @IBAction func btnAction(sender: UIButton){
         print("sender.tag\(sender.tag)")
-        btn(sender:sender, id:myid, ex:ex[0])
-        bomb(sender:sender, id:myid, ex:ex[0])
+        btn(sender:sender, id:myid, ex:ex[0], point:point[0])
+        if(sender.tag==5){
+            bomb(sender:sender, id:myid, ex:ex[0], point:point[0])
+        }
         showBoard()
         showCharacter()
     }
     
-    func bomb(sender:UIButton, id:Int, ex:Int){
-        if(sender.tag==5&&number[ex-19]==3||number[ex-18]==3||number[ex-17]==3||number[ex-1]==3||number[ex]==3||number[ex+1]==3||number[ex+17]==3||number[ex+18]==3||number[ex+19]==3||number[ex-19]==4||number[ex-18]==4||number[ex-17]==4||number[ex-1]==4||number[ex]==4||number[ex+1]==4||number[ex+17]==4||number[ex+18]==4||number[ex+19]==4){
-                        //t=1
-                        //ポイント処理
-            doPost(id:id,num:ex,tag:sender.tag)
-                        //self.performSegue(withIdentifier: "toLastVC", sender: nil)
+    func bomb(sender:UIButton, id:Int, ex:Int, point:Int){
+        var p=point
+        if(number[ex-19]==3||number[ex-18]==3||number[ex-17]==3||number[ex-1]==3||number[ex]==3||number[ex+1]==3||number[ex+17]==3||number[ex+18]==3||number[ex+19]==3){
+            if(p==0||p==2){
+                p+=1
+                doPost(id:id,num:ex,tag:sender.tag,point:p)
+            }
+        }else if(number[ex-19]==4||number[ex-18]==4||number[ex-17]==4||number[ex-1]==4||number[ex]==4||number[ex+1]==4||number[ex+17]==4||number[ex+18]==4||number[ex+19]==4){
+            if(p==0||p==1){
+                p+=2
+                doPost(id:id,num:ex,tag:sender.tag,point:p)
+            }
+        }
+        //self.performSegue(withIdentifier: "toLastVC", sender: nil)
+    }
+}
+
+//プロトコル
+extension GameViewController: AbstractClass {
+    func task() {
+        switch point[0]{
+        case 0:
+            let image1 = UIImage(named: "jinro1")
+            jinro.image = image1
+        case 1:
+            let image1 = UIImage(named: "jinro2")
+            jinro.image = image1
+        case 2:
+            let image1 = UIImage(named: "jinro3")
+            jinro.image = image1
+        case 3:
+            let image1 = UIImage(named: "jinro4")
+            jinro.image = image1
+        default:break
         }
     }
 }
